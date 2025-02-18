fbebeeb14c02f27ea27981d4f2fdb5e1a4a25489
mbali-18 <faithfulservent18@gmail.com>
2025-02-18 23:42:08 +0200

    tailwind

---
+++ tailwind-project/src/App.jsx
@@ -0,0 +1,17 @@
+import "./App.css";
+
+const App = () => {
+  return (
+    <div>
+      <section className="h-screen bg-green-400 flex items-center justify-center m-12 text-center text-3xl p-12 md:p-14 lg:p-16 xl:p-20" >
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.
+           Architecto voluptatem adipisci minima, perferendis ea 
+           repellendus facilis dolor necessitatibus amet iure, 
+           quaerat delectus tempora placeat doloribus cupiditate
+            molestias fugit, explicabo doloremque!</p>
+      </section>
+    </div>
+  )
+}
+
+export default App
